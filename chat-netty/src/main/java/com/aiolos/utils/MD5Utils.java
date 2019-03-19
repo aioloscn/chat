a7c093d23e16cc449fe1a6d76b3b609fe507a96d
aiolos <15988346908@163.com>
2019-03-19 12:53:23 +0800

    add utils

---
+++ chat-netty/src/main/java/com/aiolos/utils/MD5Utils.java
@@ -0,0 +1,21 @@
+package com.aiolos.utils;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.security.MessageDigest;
+
+/**
+ * @author Aiolos
+ * @date 2019-03-19 12:31
+ */
+public class MD5Utils {
+
+    /**
+     * @Description: 对字符串进行md5加密
+     */
+    public static String getMD5Str(String strValue) throws Exception {
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        String newstr = Base64.encodeBase64String(md5.digest(strValue.getBytes()));
+        return newstr;
+    }
+}
